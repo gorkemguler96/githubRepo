8f25eac3cb7fa6fea035600a0bb6e291068a179e
Görkem GÜLER <gorkemguler1996@gmail.com>
2022-06-07 15:13:38 +0300

    motion alıştırmaları tamamlandi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion, useAnimation } from "framer-motion";
+import {useRef} from "react";
 
 function App() {
+
+    const controls = useAnimation();
+    const containerRef = useRef(null)
+    const variants = {
+        visible:{
+            opacity:1,
+            transition: {
+                when:'beforeChildren',
+                staggerChildren: 0.3,
+            }
+        },
+        hidden:{
+            opacity:0
+        }
+    }
+
+    const item = {
+        visible:{
+            opacity:1,
+            y:0,
+
+        },
+        hidden:{
+            opacity:0,
+            y:100,
+        }
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"container"} ref={containerRef}>
+        {/*<motion.div*/}
+        {/*    className={"box"}*/}
+        {/*    animate={{*/}
+        {/*        scale:[0,1.2,1.8,.2],*/}
+        {/*        borderRadius:['20%','30%', '50%', '20%'],*/}
+        {/*        rotate: [90,180,270]*/}
+        {/*    }}*/}
+        {/*    // drag*/}
+        {/*    // whileHover={{*/}
+        {/*    //     scale:2*/}
+        {/*    // }}*/}
+        {/*    // whileTap={{*/}
+        {/*    //     rotate:360*/}
+        {/*    // }}*/}
+        {/*    // dragConstraints={containerRef}*/}
+        {/*    // dragElastic={false}*/}
+        {/*    initial={{*/}
+        {/*        // borderRadius:'20%'*/}
+        {/*    }}*/}
+        {/*    // animate={{*/}
+        {/*    //     rotate:360,*/}
+        {/*    //     borderRadius:'20%',*/}
+        {/*    //     scale:1.3*/}
+        {/*    // }}*/}
+        {/*    transition={{*/}
+        {/*        duration:4,*/}
+        {/*        // repeat:Infinity,*/}
+        {/*    }}*/}
+        {/*/>*/}
+
+
+
+        {/*<motion.ul*/}
+        {/*    className={"box"}*/}
+        {/*    initial={"hidden"}*/}
+        {/*    animate={"visible"}*/}
+        {/*    variants={variants}*/}
+        {/*>*/}
+        {/*    <motion.li style={{*/}
+        {/*        marginBottom:20,*/}
+        {/*        width:20,*/}
+        {/*        height:20,*/}
+        {/*        backgroundColor:'blue',*/}
+        {/*    }}*/}
+        {/*    variants={item}*/}
+        {/*    />*/}
+        {/*    <motion.li style={{*/}
+        {/*        marginBottom:20,*/}
+        {/*        width:20,*/}
+        {/*        height:20,*/}
+        {/*        backgroundColor:'blue',*/}
+        {/*    }}*/}
+        {/*    variants={item}*/}
+        {/*    />*/}
+        {/*</motion.ul>*/}
+
+
+
+    <motion.div className="box" animate={controls}>
+        <button onClick={()=>controls.start({
+            x:200,
+            transition:{
+                type:'spring',
+                bounce:0.8
+            },
+        })}>Start</button>
+        <button onClick={()=>controls.stop({
+            scale:4
+        })}>Stop</button>
+    </motion.div>
+
     </div>
   );
 }
